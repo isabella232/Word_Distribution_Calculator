c8c8db22f6de62804a59941c6d5d4d1ea17602a3
jhvoong <jhvoong@ucdavis.edu>
2019-09-19 01:03:45 -0700

    Add files via upload

---
+++ student_starter/word_distribution_calculator_ss.py
@@ -0,0 +1,59 @@
+def calc_distribution(text, word):
+    ''' Calculates the percentage of how many times word appears in text
+        Percentage = number of times word appears / total amount of words * 100
+
+        Input: "cat cat cat dog", "dog"
+        Output: 25 (Since it is 25%)
+
+        Input: "tomato tomatoes tomato", "tomato"
+        Output: 66.66
+    '''
+
+    # gather appropriate data
+
+    # check if word appears at all
+
+    # calculate percentage and return it
+
+def stringToWords(text):
+    ''' Separates a string into a list of individual words.
+        Words are denoted as having space in-between them.
+        Assumes no duplicate spaces and no punctuation
+        
+        Input: "An example sentence"
+        Output: ["An", "example", "sentence"]
+    '''
+    # initialize variables
+
+    # indices for slicing text
+
+    # iterate through each character, finding appropriate slicing ranges
+
+
+    # add remaining characters as the last word
+
+
+
+def word_counter(text):
+    ''' Counts the number of words in text
+        Returns a list containing two elements:
+            a dictionary of words and their frequencies
+            the total amount of words
+
+        Input: "cat cat cat dog"
+        Output: [ {("cat" : 3), ("dog" : 1)}, 4 ]
+    '''
+        
+    # format the raw string into a list of words
+
+    # calculate the frequency of each word and total word count
+
+
+def main():
+    # get raw text
+
+    # get word
+
+    # calculate word distribution in raw text
+
+    # print result
